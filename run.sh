a9a165a8dd6697d4af983fe9aa0e582502a31e4d
chLi <565353780@qq.com>
2023-08-16 03:57:29 +0800

    update model file path, train success

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 cd ../qlr
 python qlora.py \
-	--model_name_or_path /home/chli/github/textgen/models/llama-2-7B-Guanaco-QLoRA-GPTQ/ \
-	--dataset /home/chli/github/textgen/training/datasets/data1_train.json \
+	--model_name_or_path /home/chli/models/llama-2-7b-chat-hf/ \
+	--dataset /home/chli/chLi/Sql-Dataset/data_test1_train.json \
 	--dataset_formar alpaca
