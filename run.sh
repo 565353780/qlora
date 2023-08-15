779a4df3890257a22fbf192319fac8f282758698
chLi <565353780@qq.com>
2023-08-16 04:10:03 +0800

    add more params for training

--- run.sh
+++ run.sh
@@ -2,4 +2,7 @@ cd ../qlr
 python qlora.py \
 	--model_name_or_path /home/chli/models/llama-2-7b-chat-hf/ \
 	--dataset /home/chli/chLi/Sql-Dataset/data_test1_train.json \
-	--dataset_formar alpaca
+	--dataset_formar alpaca \
+	--learning_rate 1e-4 \
+	--save_steps 10 \
+	--save_total_limit 100
