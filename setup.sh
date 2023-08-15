3e47a0e8de1eb0ae642d5ebb7d4cb1dbcb89733a
chLi <565353780@qq.com>
2023-08-16 04:09:58 +0800

    upgrade necessary packages into latest

--- setup.sh
+++ setup.sh
@@ -6,3 +6,8 @@ git pull
 
 pip install -U torch torchvision torchaudio
 pip install -U -r requirements.txt
+
+pip install -U bitsandbytes
+pip install -U git+https://github.com/huggingface/transformers.git
+pip install -U git+https://github.com/huggingface/peft.git
+pip install -U git+https://github.com/huggingface/accelerate.git
