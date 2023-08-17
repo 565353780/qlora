22f4cc88ccc93c3713d1636ce59f693d6cdafbe6
chLi <565353780@qq.com>
2023-08-18 03:20:57 +0800

    set continuous training

--- train.sh
+++ train.sh
@@ -3,6 +3,7 @@ python qlora.py \
 	--model_name_or_path /home/chli/github/textgen/models/chinese-alpaca-2-7b/ \
 	--dataset /home/chli/chLi/Sql-Dataset/data_test1_train.json \
 	--dataset_formar alpaca \
-	--learning_rate 1e-4 \
-	--save_steps 10 \
-	--save_total_limit 100
+	--learning_rate 2e-4 \
+	--save_steps 1000 \
+	--save_total_limit 100 \
+	--max_steps 1000000
